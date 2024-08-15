1c9b812fb98ee6ec8d2734b2827d5a185a26d6f2
Max Kriukov <max.krukov89@gmail.com>
2024-08-15 11:43:21 +0300

    AnodotLinkedAccounts

--- aws/Connect2Anodot.sh
+++ aws/Connect2Anodot.sh
@@ -1,12 +1,36 @@
-#!/bin/bash
+#!/bin/bash -e
 
-ExternalId="<external-id>"
-AnodotLinkedAccountRole="false"
+ExternalId="abcd1234"
+AnodotLinkedAccountRole="true"
+RootOUId="r-sp1e"
+
+StackSetsTrustAccess() {
+  # Check if CloudFormation StackSets trusted access is already enabled
+  SERVICE_PRINCIPAL="member.org.stacksets.cloudformation.amazonaws.com"
+
+  # List services with trusted access enabled
+  ENABLED_SERVICES=$(aws organizations list-aws-service-access-for-organization --query 'EnabledServicePrincipals[].ServicePrincipal' --output text)
+
+  # Check if StackSets is in the list
+  if echo "$ENABLED_SERVICES" | grep -q "$SERVICE_PRINCIPAL"; then
+    echo "Trusted access for CloudFormation StackSets is already enabled."
+  else
+    echo "Could you please enable trusted access for CloudFormation StackSets..."
+    # You should activate trusted access with AWS Organizations.
+    echo "https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/stacksets-orgs-activate-trusted-access.html"
+    exit 1
+
+    # Enable trusted access for CloudFormation StackSets
+    #aws organizations enable-aws-service-access --service-principal $SERVICE_PRINCIPAL
+
+    #echo "Trusted access for CloudFormation StackSets has been enabled."
+  fi
+}
 
 # If AnodotLinkedAccountRole="true"
 # You should activate trusted access with AWS Organizations.
 # https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/stacksets-orgs-activate-trusted-access.html
-RootOUId="r-XXXX"   # for example: "r-sp1n"
+[ $AnodotLinkedAccountRole == "true" ] && StackSetsTrustAccess
 
 aws --region us-east-1 cloudformation create-stack --stack-name Anodot-Onboarding --template-body file://AnodotPayer.yaml \
                 --parameters ParameterKey=ExternalId,ParameterValue=${ExternalId} \
